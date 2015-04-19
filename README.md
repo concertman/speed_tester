007739f313178d08128f64147160a676ee2b323c
Jarod McBride <jarod@ustwo.com>
2015-04-19 12:42:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## speed_tester
+
+A small system that I use to measure my home internet connection speed.
+
+#### Requirements
+
+speedtest-cli
+```
+easy_install speedtest-cli
+```
+
+#### Install
+
+```ruby
+bundle install
+```
+
+#### Run
+
+in cron
+
+```shell
+ruby path/to/speed_test.rb
+```
---
+++ speed_test.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require 'open3'
+require 'sqlite3'
+require 'date'
+
+COMMAND = 'speedtest-cli --simple'
+
+output = ''
+
+Open3.popen3(COMMAND) do |_stdin, stdout, _stderr, _wait_thr|
+  output = stdout.read
+end
+
+puts "output == #{output}"
+
+begin
+  db = SQLite3::Database.new('speed_test.db')
+  db.execute "CREATE TABLE IF NOT EXISTS Speed_Tests(id INTEGER PRIMARY KEY, date NUMERIC, ping REAL, upload REAL, download REAL)"
+  db.execute "INSERT INTO Speed_Tests(date, ping, upload, download) VALUES ('#{DateTime.now}', '#{output.split[1]}', '#{output.split[4]}', '#{output.split[7]}')"
+rescue SQLite3::Exception => e
+  puts 'Exception occurred'
+  puts e
+ensure
+  db.close if db
+end
